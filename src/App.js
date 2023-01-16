a9fdb74ba0f46fbd022335234d6473288b6609c1
zaaiiin <zaainnaseem@gmail.com>
2023-01-17 00:12:03 +0800

    design landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from "./logo.svg";
+import confetti from "./assets/logowithconfetti.png";
+import title from "./assets/CardGen.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={title} alt="title" id="title" />
+        <div className="mainImg">
+          <img src={confetti} alt="confettiwithlogo" id="confettiwithlogo" />
+        </div>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span class="lightGreen">Birthday </span>
+          <span class="darkGreen">Reminders</span> and
+          <span class="lightGreen"> Greeting </span>
+          <span class="darkGreen">Cards</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
