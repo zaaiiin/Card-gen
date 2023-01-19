eb24383799b828bc822916225f85522b59d968be
zaaiiin <zaainnaseem@gmail.com>
2023-01-19 23:04:51 +0800

    add sign up form

---
+++ src/containers/createaccount/CreateAccount.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./createaccount.css";
+
+const CreateAccount = () => {
+  return (
+    <div className="box">
+      <div className="form_container">
+        <div className="form">
+          <label class="switch">
+            <input type="checkbox" />
+            <span class="slider round"></span>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateAccount;
