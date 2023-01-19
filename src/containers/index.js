e1b48d0ba0dd6a840cca9fd6aae618c0db1abef8
zaaiiin <zaainnaseem@gmail.com>
2023-01-19 20:06:38 +0800

    export CreateAccount

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,2 +1,3 @@
 export { default as Landingpage } from "./landingpage/Landingpage";
 export { default as About } from "./about/About";
+export { default as CreateAccount } from "./createaccount/CreateAccount";
