e7bdb24beb4db18a74539f53b229edd688b069bc
zaaiiin <zaainnaseem@gmail.com>
2023-02-02 20:30:41 +0800

    export dashboard component

--- src/containers/index.js
+++ src/containers/index.js
@@ -3,3 +3,4 @@ export { default as About } from "./about/About";
 export { default as Form } from "./form/Form";
 export { default as UpcomingEvents } from "./upcomingevents/UpcomingEvents";
 export { default as Addevent } from "./addevent/Addevent";
+export { default as Dashboard } from "./dashboard/Dashboard";
