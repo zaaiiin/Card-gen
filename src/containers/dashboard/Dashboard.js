6606fd97ced2785f7d497bf5d912138826ff1e36
zaaiiin <zaainnaseem@gmail.com>
2023-02-09 17:42:12 +0800

    add corresponding icons

--- src/containers/dashboard/Dashboard.js
+++ src/containers/dashboard/Dashboard.js
@@ -1,27 +1,36 @@
 import React from "react";
 import "./dashboard.css";
 import balloon from "../../assets/balloon.png";
-// import heart from "../../assets/heart.png";
-// import otherevent from "../../assets/otherevent.png";
+import heart from "../../assets/heart.png";
+import otherevent from "../../assets/otherevent.png";
 
 const Dashboard = (props) => {
+  let arrayOfIcons = [];
+
+  arrayOfIcons.push(
+    <img src={balloon} />,
+    <img src={heart} />,
+    <img src={otherevent} />
+  );
+  console.log(arrayOfIcons);
+
+  function displayIcon(data) {
+    let eventIcon;
+    if (data.birthday) {
+      eventIcon = <img src={balloon} alt="balloon" />;
+    } else if (data.anniversary) {
+      eventIcon = <img src={heart} alt="heart" />;
+    } else if (data.otherevent) {
+      eventIcon = <img src={otherevent} alt="otherevent" />;
+    } else {
+      eventIcon = "";
+    }
+    return eventIcon;
+  }
+
   const { submittedData } = props;
   console.log(submittedData);
   return (
-    // <div className="dashboardContainer">
-    //   <ul>
-    //   {submittedData.map((data, index) => {
-    //     return (
-    //       <div className="allContainers" key={index}>
-    //         {data.firstname} {data.lastname} {data.birthday} &nbsp; &nbsp;
-    //         {data.anniversary} {data.otherevent} &nbsp;
-    //         {data.othereventdate}
-    //       </div>
-    //     );
-    //   })}
-    //   </ul>
-    // </div>
-
     <div className="dashboardContainer">
       <ul>
         {submittedData.map((data, index) => {
@@ -37,9 +46,10 @@ const Dashboard = (props) => {
                 <div className="dashboardContent dateSideContainer"></div>{" "}
               </div>
               <div className="dashboardContent eventName">
-                <div className="eventIcon">
-                  <img src={balloon} className="iconImage" id="balloon"></img>
+                <div className="eventIcon" key={data.index}>
+                  {displayIcon(data)}
                 </div>
+
                 <div className="eventType">
                   {data.firstname} {data.lastname}
                   {data.otherevent}
@@ -55,16 +65,5 @@ const Dashboard = (props) => {
     </div>
   );
 };
-
-/* <div className="allButtons">
-
-      
-  
-<button className="allClear" onClick={allClear}>AC</button>
-
-
-
-<button className="operators" onClick={operatorType}>/</button>
-<button className="digits" onClick={inputNum} >7</button> */
 
 export default Dashboard;
